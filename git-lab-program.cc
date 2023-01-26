f5df168e70265783c914b4f435af292c62cacc2a
Aiden Schmoll <as325721@ohio.edu>
2023-01-25 19:40:47 -0500

    Hello Git

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Aiden Schmoll>
+ *        Date: <01/25/2023>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
